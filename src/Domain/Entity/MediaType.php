f2f24c3ebb311a0d249239d15cd2ba2cfeed7526
sjeguedes <sje.guedes@gmail.com>
2019-02-12 12:03:10 +0100

    finished trick list with load more functionality - added a complete page list called tricks with advanced php pagination - TODO: need to create unique slug for each trick with encoded uuid

---
+++ src/Domain/Entity/MediaType.php
@@ -0,0 +1,224 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Domain\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Ramsey\Uuid\Uuid;
+use Ramsey\Uuid\UuidInterface;
+
+/**
+ * MediaType entity.
+ *
+ * Define MediaType entity schema in database, its initial state and behaviors.
+ *
+ * @ORM\Entity(repositoryClass=MediaTypeRepository::class)
+ * @ORM\Table(name="media_types")
+ */
+class MediaType
+{
+    /**
+     * The internal primary identity key.
+     *
+     * @var UuidInterface
+     *
+     * @ORM\Id()
+     * @ORM\Column(type="uuid_binary", unique=true)
+     */
+    private $uuid;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", unique=true)
+     */
+    private $name;
+
+    /**
+     *
+     * @var string
+     *
+     * @ORM\Column(type="string")
+     */
+    private $description;
+
+    /**
+     * @var \DateTimeInterface
+     *
+     * @ORM\Column(type="datetime")
+     */
+    private $creationDate;
+
+    /**
+     * @var \DateTimeInterface
+     *
+     * @ORM\Column(type="datetime")
+     */
+    private $updateDate;
+
+    /**
+     * @var Collection (inverse side of entity relation)
+     *
+     * @ORM\OneToMany(targetEntity=Media::class, cascade={"persist", "remove"}, orphanRemoval=true, mappedBy="mediaType")
+     */
+    private $medias;
+
+    /**
+     * MediaType constructor.
+     *
+     * @param string                  $name
+     * @param string                  $description
+     * @param \DateTimeInterface|null $creationDate
+     * @param \DateTimeInterface|null $updateDate
+     *
+     * @return void
+     *
+     * @throws \Exception
+     */
+    public function __construct(
+        string $name,
+        string $description,
+        \DateTimeInterface $creationDate = null,
+        \DateTimeInterface $updateDate = null
+    ) {
+        $this->uuid = Uuid::uuid4();
+        assert(!empty($name),'MediaType name can not be empty!');
+        $this->name = $name;
+        assert(!empty($description),'MediaType description can not be empty!');
+        $this->description = $description;
+        $createdAt = !\is_null($creationDate) ? $creationDate :  new \DateTime('now');
+        $this->creationDate = $createdAt;
+        assert($updateDate > $this->creationDate,'MediaType can not be created after update date!');
+        $this->updateDate = !\is_null($updateDate) ? $updateDate : $this->creationDate;
+        $this->medias = new ArrayCollection();
+    }
+
+    /**
+     * Change name after creation.
+     *
+     * @param string $name
+     *
+     * @return MediaType
+     */
+    public function modifyName(string $name) : self
+    {
+        if (empty($name)) {
+            throw new \InvalidArgumentException('MediaType name can not be empty!');
+        }
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+    * Change description after creation.
+    *
+    * @param string $description
+    *
+    * @return MediaType
+    */
+    public function modifyDescription(string $description) : self
+    {
+        if (empty($description)) {
+            throw new \InvalidArgumentException('MediaType description can not be empty!');
+        }
+        $this->description = $description;
+        return $this;
+    }
+
+    /**
+     * Change update date after creation.
+     *
+     * @param \DateTimeInterface $updateDate
+     *
+     * @return MediaType
+     */
+    public function modifyUpdateDate(\DateTimeInterface $updateDate) : self
+    {
+        if ($this->updateDate > $updateDate) {
+            throw new \RuntimeException('Update date is not logical: MediaType can not be created after modified update date!');
+        }
+        $this->updateDate = $updateDate;
+        return $this;
+    }
+
+    /**
+     * Add Media entity to collection.
+     *
+     * @param Media $media
+     *
+     * @return MediaType
+     */
+    public function addMedia(Media $media) : self
+    {
+        if (!$this->medias->contains($media)) {
+            $this->medias->add($media);
+            $media->modifyMediaType($this);
+        }
+        return $this;
+    }
+
+    /**
+     * Remove Media entity from collection.
+     *
+     * @param Media $media
+     *
+     * @return MediaType
+     */
+    public function removeMedia(Media $media) : self
+    {
+        if ($this->medias->contains($media)) {
+            $this->medias->removeElement($media);
+        }
+        return $this;
+    }
+
+    /**
+     * @return UuidInterface
+     */
+    public function getUuid() : UuidInterface
+    {
+        return $this->uuid;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName() : string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription() : string
+    {
+        return $this->description;
+    }
+
+    /**
+     * @return \DateTimeInterface
+     */
+    public function getCreationDate() : \DateTimeInterface
+    {
+        return $this->creationDate;
+    }
+
+    /**
+     * @return \DateTimeInterface
+     */
+    public function getUpdateDate() : \DateTimeInterface
+    {
+        return $this->updateDate;
+    }
+
+    /**
+     * @return Collection|Media[]
+     */
+    public function getMedias() : Collection
+    {
+        return $this->medias;
+    }
+}
---
+++ src/Utils/Database/DataFixtures/TrickGroupFixtures.php
@@ -0,0 +1,41 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Utils\Database\DataFixtures;
+
+use App\Domain\Entity\TrickGroup;
+use Doctrine\Common\Persistence\ObjectManager;
+
+/**
+ * Class TrickGroupFixtures.
+ *
+ * Generate trick group fake starting set of data in database.
+ */
+class TrickGroupFixtures extends BaseFixture
+{
+    /**
+     * Persist and flush entities in database.
+     *
+     * @param ObjectManager $manager
+     *
+     * @return void
+     *
+     * @throws \Exception
+     */
+    public function loadData(ObjectManager $manager) : void
+    {
+        $array = $this->parseYamlFile('trick_group_fixtures.yaml');
+        $data = $array['trick_groups'];
+        // Create trick groups
+        $this->createFixtures(TrickGroup::class, \count($data), function($i) use($data) {
+            $trickGroup = new TrickGroup(
+                $data[$i]['fields']['name'],
+                $data[$i]['fields']['description'],
+                new \DateTime(sprintf("+%d days", $i - 1)),
+                new \DateTime(sprintf("+%d days", $i - 1))
+            );
+            return $trickGroup;
+        });
+        $manager->flush();
+    }
+}
